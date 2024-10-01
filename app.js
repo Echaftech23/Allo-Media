04dada96792278a3304f19a700d292565a058501
unknown <rachidchaf2001@gmail.com>
2024-10-01 15:56:49 +0100

    added readme file.

--- app.js
+++ app.js
@@ -28,12 +28,14 @@ app.use('/auth', authRoutes);
 // app.use('/delivery', deliveryRoutes);
 // app.use('/manager', managerRoutes);
 
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
   });
 }
-
-
 
 module.exports = app;
